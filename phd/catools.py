e4ce3f24286372ba5cb5b88367a4bb72b7efb0fd
D3M80L <marcin.dembowski@gmail.com>
2022-12-28 19:31:41 +0100

    Updated examples

--- phd/catools.py
+++ phd/catools.py
@@ -2,14 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from IPython.display import HTML
-
+from decimal import *
 
 def acca2dVonNeumannLocalRule(lut: np.ndarray, x, y, z, u, v):
-    mx = 1.0 - x
-    my = 1.0 - y
-    mz = 1.0 - z
-    mu = 1.0 - u
-    mv = 1.0 - v
+    dec1 = Decimal('1')
+    mx = dec1 - x
+    my = dec1 - y
+    mz = dec1 - z
+    mu = dec1 - u
+    mv = dec1 - v
 
     tmp = mx * my
     result = lut[0] * tmp * mz * mu * v
@@ -50,16 +51,17 @@ def acca2dVonNeumannLocalRule(lut: np.ndarray, x, y, z, u, v):
     result += lut[29] * tmp * z * u * mv
     result += tmp * z * u * v
 
-    if (result > 1):
-        return 1
+    if (result > dec1):
+        return dec1
 
     return result
 
 
 def acca1dLocalRule(lut: np.ndarray, x, y, z):
-    mx = 1 - x
-    my = 1 - y
-    mz = 1 - z
+    dec1 = Decimal('1')
+    mx = dec1 - x
+    my = dec1 - y
+    mz = dec1 - z
 
     result = lut[0] * mx * my * mz
     result += lut[1] * mx * my * z
@@ -70,8 +72,8 @@ def acca1dLocalRule(lut: np.ndarray, x, y, z):
     result += lut[6] * x * y * mz
     result += lut[7] * x * y * z
 
-    if (result > 1):
-        return 1
+    if (result > dec1):
+        return dec1
 
     return result
 
@@ -92,16 +94,21 @@ def applyAcca2dVonNeumannRule(lut: np.ndarray, inputConfiguration: np.ndarray, o
                                                                         # center
                                                                         inputConfiguration[center + x],
                                                                         # right
-                                                                        inputConfiguration[center + (x + 1) % X],
+                                                                        inputConfiguration[center + (
+                                                                            x + 1) % X],
                                                                         inputConfiguration[bottom + x])
+
+
 def applyAcca1dRule(lut: np.ndarray, inputConfiguration: np.ndarray, outputConfiguration: np.ndarray, X: int):
     for centerIndex in range(X):
         leftIndex = (X + centerIndex - 1) % X
         rightIndex = (centerIndex + 1) % X
 
-        result = acca1dLocalRule(lut, inputConfiguration[leftIndex], inputConfiguration[centerIndex], inputConfiguration[rightIndex])
+        result = acca1dLocalRule(
+            lut, inputConfiguration[leftIndex], inputConfiguration[centerIndex], inputConfiguration[rightIndex])
 
         outputConfiguration[centerIndex] = result
+
 
 def iterate_1dconfiguration(I: np.ndarray, lut: np.ndarray):
     I1 = I.copy()
@@ -115,6 +122,7 @@ def iterate_1dconfiguration(I: np.ndarray, lut: np.ndarray):
         I2 = I1
         I1 = tmp
 
+
 def iterate_1dconfiguration_to(I: np.ndarray, lut: np.ndarray, T: int):
     iteration = iterate_1dconfiguration(I, lut)
 
@@ -122,7 +130,7 @@ def iterate_1dconfiguration_to(I: np.ndarray, lut: np.ndarray, T: int):
         yield next(iteration)
 
 
-def plot_timespatial(I: np.ndarray, ax=None):
+def plot_spacetime(I: np.ndarray, ax=None):
     if (not ax):
         ax = plt.subplot(111)
 
@@ -151,19 +159,24 @@ def iterate_configuration(I: np.ndarray, lut: np.ndarray):
         I2 = I1
         I1 = tmp
 
+
 def iterate_configuration_to(I: np.ndarray, lut: np.ndarray, T: int):
     iteration = iterate_configuration(I, lut)
 
     for t in range(T):
         yield next(iteration)
 
+
 def parse_1d_configuration(configuration: str):
-    result = [float(x) for x in configuration.split(";")]
+    c = configuration
+    if (";" in configuration):
+        c = c.split(";")
+    result = [Decimal(x) for x in c]
     return np.array(result)
 
 
 def parse_2d_configuration(configuration: str, x: int,  y: int):
-    result = [float(x) for x in configuration.split(";")]
+    result = [Decimal(x) for x in configuration.split(";")]
     return np.array(result).reshape(y, x)
 
 
@@ -202,20 +215,21 @@ def title_timespatial_default(t, configuration):
         maxEq = '>'
     elif (max < maxValue):
         maxEq = '<'
-        
+
     maxHalf = '=0.5'
     if (max > 0.5):
         maxHalf = '>0.5'
     elif (max < 0.5):
         maxHalf = '<0.5'
-        
+
     minHalf = '=0.5'
     if (min > 0.5):
         minHalf = '>0.5'
     elif (min < 0.5):
-        minHalf = '<0.5'  
+        minHalf = '<0.5'
 
     return f't={t}, $\\rho$={np.mean(configuration):1.3f}, min{minEq}{minValue:1.3f}({minHalf}), max{maxEq}{maxValue:1.3f}({maxHalf})'
+
 
 def title_notify_when_threshold_reached(threshold):
     def notify_below(t, configuration, threshold):
@@ -228,16 +242,18 @@ def title_notify_when_threshold_reached(threshold):
             return f' threshold'
         return ''
 
-    return lambda t,configuration: title_timespatial_default(t, configuration) + notify_below(t, configuration, threshold)
+    return lambda t, configuration: title_timespatial_default(t, configuration) + notify_below(t, configuration, threshold)
+
 
 def set_title(ax, timespatial, t, title=None):
-    
+
     if (title is None):
         title = title_timespatial_default
 
     result = title(t, timespatial[t])
 
     ax.set_title(result)
+
 
 def animate_iterate_configuration_to(configuration: np.ndarray, lut: np.ndarray, T: int, title=None):
     fig = plt.figure()
@@ -252,6 +268,7 @@ def animate_iterate_configuration_to(configuration: np.ndarray, lut: np.ndarray,
         plt.close()
 
     return HTML(animation.FuncAnimation(fig, animate, frames=T, interval=200, blit=False).to_jshtml())
+
 
 def plot_polar_configuration(configuration: np.ndarray, ax=None):
     if (not ax):
@@ -295,45 +312,145 @@ def animate_iterate_polar_configuration_to(configuration: np.ndarray, lut: np.nd
 
 
 def outer_totalistic_acca_lut(a, b, c, d) -> np.ndarray:
-    return np.array([a, b, c, d, b, 2*b-a, d, 2*d-c])
+    a = Decimal(a)
+    b = Decimal(b)
+    c = Decimal(c)
+    d = Decimal(d)
+    dec2 = Decimal('2')
+    return np.array([a, b, c, d, Decimal(b), dec2*b-a, d, dec2*d-c])
+
 
 def density_conserving_2d_lut(a, b, c, d, e, f, g, h) -> np.ndarray:
+    a = Decimal(a)
+    b = Decimal(b)
+    c = Decimal(c)
+    d = Decimal(d)
+    e = Decimal(e)
+    f = Decimal(f)
+    g = Decimal(g)
+    h = Decimal(h)
     return np.array([a,
-                       b,
-                       c,
-                       d,
-                       e,
-                       f,
-                       -a-b+c-d+e+f,
-                       g,
-                       h,
-                       b+g,
-                       -a+c+h,
-                       b+2*d-f+g,
-                       -a+b+d+e-f+h,
-                       b+d+g,
-                       -2*a+c+e+h,
-                       1-a-b-d-g,
-                       1-b - d-g,
-                       1-d - h,
-                       1-b + c - d - h,
-                       1-b + d - e - g,
-                       1-b - g,
-                       1+a - b - e + f - h,
-                       1-2*b + c - d + f - h,
-                       1-c - d,
-                       1-c - d - g + h,
-                       1+a + b - c - d - h + g,
-                       1-d,
-                       1+a + b - c + 2*d - e - f,
-                       1+b - c + d - f - g + h,
-                       1+2*a + b - c + d - e + g - h])
+                     b,
+                     c,
+                     d,
+                     e,
+                     f,
+                     -a-b+c-d+e+f,
+                     g,
+                     h,
+                     b+g,
+                     -a+c+h,
+                     b+2*d-f+g,
+                     -a+b+d+e-f+h,
+                     b+d+g,
+                     -2*a+c+e+h,
+                     1-a-b-d-g,
+                     1-b - d-g,
+                     1-d - h,
+                     1-b + c - d - h,
+                     1-b + d - e - g,
+                     1-b - g,
+                     1+a - b - e + f - h,
+                     1-2*b + c - d + f - h,
+                     1-c - d,
+                     1-c - d - g + h,
+                     1+a + b - c - d - h + g,
+                     1-d,
+                     1+a + b - c + 2*d - e - f,
+                     1+b - c + d - f - g + h,
+                     1+2*a + b - c + d - e + g - h])
+
+def density_conserving_rotation_symmetric_2d_lut(alpha:float) -> np.ndarray:
+    return np.array([
+        alpha,
+        alpha,
+        2*alpha,
+        1-4*alpha,
+        1-3*alpha,
+        1-3*alpha,
+        1-2*alpha,
+        alpha,
+        2*alpha,
+        2*alpha,
+        3*alpha,
+        1-3*alpha,
+        1-2*alpha,
+        1-2*alpha,
+        1-alpha,
+        1-4*alpha,
+        2*alpha,
+        2*alpha,
+        3*alpha,
+        1-3*alpha,
+        1-2*alpha,
+        1-2*alpha,
+        1-alpha,
+        2*alpha,
+        3*alpha,
+        3*alpha,
+        4*alpha,
+        1-2*alpha,
+        1-alpha,
+        1-alpha
+    ])
 
 def diffusion_2d_lut() -> np.ndarray:
-    return density_conserving_2d_lut(a=1/5, b=1/5, c=2/5, d=1/5, e=2/5, f=2/5, g=1/5, h=2/5)
+    dec5 = Decimal('5')
+    dec1 = Decimal('1')
+    dec2 = Decimal('2')
+    return density_conserving_2d_lut(a=dec1/dec5, b=dec1/dec5, c=dec2/dec5, d=dec1/dec5, e=dec2/dec5, f=dec2/dec5, g=dec1/dec5, h=dec2/dec5)
+
 
 def diffusion_1d_lut() -> np.ndarray:
-    return np.array([0, 1/3, 1/3, 2/3, 1/3, 2/3, 2/3, 1])
+    dec0 = Decimal('0')
+    dec1 = Decimal('1')
+    dec2 = Decimal('2')
+    dec3 = Decimal('3')
+    return np.array([dec0, dec1/dec3, dec1/dec3, dec2/dec3, dec1/dec3, dec2/dec3, dec2/dec3, dec1])
+
 
 def density_conserving_rotation_symmetric_2d_lut(alpha) -> np.ndarray:
+    if (alpha < 0 or alpha > 0.25):
+        raise ValueError('The alpha value can be only in range [0, 0.25].')
+
     return density_conserving_2d_lut(a=alpha, b=alpha, c=2*alpha, d=1-4*alpha, e=1-3*alpha, f=1-3*alpha, g=alpha, h=2*alpha)
+
+def density_conserving_1d_lut(a, b, c) -> np.ndarray:
+    a = Decimal(a)
+    b = Decimal(b)
+    c = Decimal(c)
+    dec0 = Decimal('0')
+    dec1 = Decimal('1')
+    return np.array([
+        dec0,
+        a,
+        c,
+        dec1-b,
+        dec1-a-c,
+        dec1-c,
+        b+c,
+        dec1])
+
+def truncate(x):
+    truncated = Decimal(int(x * 100000)) / Decimal(100000)
+    dots = ""
+    if (truncated != x):
+        dots = "\ldots"
+    return f"{truncated}{dots}"
+
+def display_lut(lut: np.ndarray):
+    if (len(lut) == 6 or len(lut) == 30):
+        lut = np.concatenate(([0], lut, [1]))
+    
+    if (len(lut) != 8 and len(lut) != 32):
+        print (len(lut))
+        raise ValueError('Only 1D or 2D LUT is allowed.')
+
+    result = "<table style='border: 1px solid black;border-collapse: collapse;width:100%'><caption>LUT representation</caption>"
+    for y in range(len(lut) // 8):
+        header = ''.join([f'<td style="text-align:center;border: 1px solid black">$l_{{{x+8*y}}}$</td>' for x in range(8)])
+        values = ''.join([f'<td style="text-align:center;border: 1px solid black">${truncate(x)}$</td>' for x in lut[y*8:y*8+8]])
+        result += f'<tr>{header}</tr><tr>{values}</tr>'
+    result += "</table>"
+
+    return HTML(result)
